c6837169a7f0de4c4d336d90e88cfdea2f32a0b4
lzl2040 <2040171586@qq.com>
2024-04-05 23:25:42 +0800

    Initial commit

---
+++ Exp1/cnn.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+"""
+File cnn.py
+Created on 2024/4/5 23:16
+Copyright (c) 2024/4/5
+@author: 
+"""
---
+++ Exp1/train.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+"""
+File train.py.py
+Created on 2024/4/5 23:16
+Copyright (c) 2024/4/5
+@author: 
+"""
